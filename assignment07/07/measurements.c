d4f7dd3f446b6867aff036a39739fedccde54e7d
Hamza Mirza <hamza665@hotmail.de>
2025-12-11 14:32:53 +0100

    removed one liner to make code more readable

--- assignment07/07/measurements.c
+++ assignment07/07/measurements.c
@@ -51,53 +51,117 @@ void print_statistics(Statistics s) {
 Statistics compute_statistics(String table) {
     // todo
     Statistics stats = make_statistics();
+
     int len = s_length(table);
     int pos = 0;
 
     int count_total = 0;
-    double sum_age = 0;
-    double sum_age_sq = 0;
+    double sum_age = 0.0;
+    double sum_age_sq = 0.0;
 
-    int count_m = 0, count_t = 0, count_k = 0;
-    double sum_m = 0, sum_t = 0, sum_k = 0;
+    int count_m = 0;
+    int count_t = 0;
+    int count_k = 0;
+
+    double sum_m = 0.0;
+    double sum_t = 0.0;
+    double sum_k = 0.0;
 
     while (pos < len) {
 
-        while (pos < len && s_get(table, pos) <= ' ') pos++;
-        if (pos >= len) break;
-        int start = pos;
-        while (pos < len && s_get(table, pos) > ' ') pos++;
-        int age = i_of_s(s_sub(table, start, pos));
+        while (pos < len && s_get(table, pos) <= ' ') {
+            pos++;
+        }
 
-        while (pos < len && s_get(table, pos) <= ' ') pos++;
-        if (pos >= len) break;
+        if (pos >= len) {
+            break;
+        }
+
+        int start = pos;
+
+        while (pos < len && s_get(table, pos) > ' ') {
+            pos++;
+        }
+
+        String age_str = s_sub(table, start, pos);
+        int age = i_of_s(age_str);
+
+        while (pos < len && s_get(table, pos) <= ' ') {
+            pos++;
+        }
+
+        if (pos >= len) {
+            break;
+        }
+
         char device = s_get(table, pos);
         pos++;
 
-        while (pos < len && s_get(table, pos) <= ' ') pos++;
+        while (pos < len && s_get(table, pos) <= ' ') {
+            pos++;
+        }
+
         start = pos;
-        while (pos < len && s_get(table, pos) > ' ') pos++;
-        double time = d_of_s(s_sub(table, start, pos));
+
+        while (pos < len && s_get(table, pos) > ' ') {
+            pos++;
+        }
+
+        String time_str = s_sub(table, start, pos);
+        double time = d_of_s(time_str);
 
         count_total++;
         sum_age += age;
-        sum_age_sq += (double) age * age;
+        sum_age_sq += (double)age * age;
 
-        if (device == 'm') { count_m++; sum_m += time; }
-        else if (device == 't') { count_t++; sum_t += time; }
-        else if (device == 'k') { count_k++; sum_k += time; }
+        if (device == 'm') {
+            count_m++;
+            sum_m += time;
+        }
+        else if (device == 't') {
+            count_t++;
+            sum_t += time;
+        }
+        else if (device == 'k') {
+            count_k++;
+            sum_k += time;
+        }
     }
 
     if (count_total > 0) {
         stats.age = sum_age / count_total;
+
         double mean_sq = sum_age_sq / count_total;
         double variance = mean_sq - stats.age * stats.age;
-        stats.age_sd = sqrt(variance > 0 ? variance : 0);
+
+        if (variance > 0.0) {
+            stats.age_sd = sqrt(variance);
+        }
+        else {
+            stats.age_sd = 0.0;
+        }
     }
 
-    stats.mean_m = (count_m > 0) ? sum_m / count_m : 0;
-    stats.mean_t = (count_t > 0) ? sum_t / count_t : 0;
-    stats.mean_k = (count_k > 0) ? sum_k / count_k : 0;
+    if (count_m > 0) {
+        stats.mean_m = sum_m / count_m;
+    }
+    else {
+        stats.mean_m = 0.0;
+    }
+
+    if (count_t > 0) {
+        stats.mean_t = sum_t / count_t;
+    }
+    else {
+        stats.mean_t = 0.0;
+    }
+
+    if (count_k > 0) {
+        stats.mean_k = sum_k / count_k;
+    }
+    else {
+        stats.mean_k = 0.0;
+    }
 
     return stats;
 }
