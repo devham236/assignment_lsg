feb688e391b7295c9bd79b00bd610396f157a95b
Hamza Mirza <hamza665@hotmail.de>
2025-12-11 12:57:00 +0100

    adjusted print statements

--- assignment07/07/measurements.c
+++ assignment07/07/measurements.c
@@ -34,6 +34,7 @@ typedef struct statistics_s {
     // todo
     Tech tech;
     double age;
+    double age_sd;
     double time;
 } Statistics;
 
@@ -42,7 +43,8 @@ Statistics make_statistics() {
     Statistics stats;
 
     stats.tech = 0;
-    stats.age = 0;
+    stats.age = 0.0;
+    stats.age_sd = 0.0;
     stats.time = 0.0;
 
     return stats;
@@ -50,15 +52,17 @@ Statistics make_statistics() {
 
 void print_statistics(Statistics s) {
     // todo
-    printf("Age (Mean): %f years\n", 37.33);
-    printf("Age (Std. Dev.): %f years\n", 11.14);
-    printf("Mean Time (Mouse): %f s\n", 16.00);
-    printf("Mean Time (Touchscreen): %f s\n", 12.86);
-    printf("Mean Time (Keyboard): %f s\n", 22.40);
+    printf("Age (Mean): %22.2f years\n", s.age);
+    printf("Age (Std. Dev.): %17.2f years\n", s.age_sd);
+    printf("Mean Time (Mouse): %15.2f s\n", s.tech);
+    printf("Mean Time (Touchscreen): %9.2f s\n", s.tech);
+    printf("Mean Time (Keyboard): %12.2f s\n", s.tech);
 }
 
 Statistics compute_statistics(String table) {
     // todo
+    int table_length = s_length(table);
+    int i = 0;
 }
 
 int main(void) {
