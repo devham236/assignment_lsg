d772cf0f3f7315d158f328707b42020b487dc985
Hamza Mirza <hamza665@hotmail.de>
2026-03-05 11:36:51 +0100

    solved monotone tree

--- 12/is_monotone_tree.c
+++ 12/is_monotone_tree.c
@@ -1,4 +1,5 @@
 #include "base.h"
+#include <limits.h>
 
 typedef struct s_Node
 {
@@ -37,25 +38,97 @@ void print_tree(Node *node, int depth)
 // TODO: Die Funktion soll überprüfen, ob ein Baum monoton ist.
 //  Ein Knoten ist monoton, wenn der Wert nicht größer ist, als die Werte der unmittelbaren Kinder.
 //  Ein leerer Baum ist immer monoton.
+
+bool is_monotone_tree_recursive(Node *node, int min)
+{
+    if (node == NULL)
+    {
+        return true;
+    }
+
+    bool left_is_ok = is_monotone_tree_recursive(node->left, node->value);
+    bool right_is_ok = is_monotone_tree_recursive(node->right, node->value);
+
+    if (node->value >= min && left_is_ok && right_is_ok)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+
+    return true;
+}
+
 bool is_monotone_tree(Node *node)
 {
-    return false;
+    return is_monotone_tree_recursive(node, INT_MIN);
 }
 
 int main()
 {
     test_equal_b(is_monotone_tree(NULL), true);
+    /*
+    NULL
+    */
+
     test_equal_b(is_monotone_tree(leaf(1)), true);
+    /*
+            1
+           / \
+          N   N
+    */
 
     Node *t0 = new_node(leaf(6), 5, leaf(2));
     test_equal_b(is_monotone_tree(t0), false);
+    /*
+            5
+           / \
+          6   2
+         / \ / \
+        N  N N  N
+    */
 
     Node *t1 = new_node(new_node(new_node(leaf(3), 5, leaf(2)), 10, new_node(leaf(1), 4, leaf(0))), 20, new_node(new_node(leaf(3), 5, leaf(2)), 8, new_node(leaf(6), 6, leaf(4))));
-    test_equal_b(is_monotone_tree(t1), true);
+    test_equal_b(is_monotone_tree(t1), false);
+    /*
+                20
+              /    \
+            10      8
+           /  \    / \
+          5    4  5   6
+         / \  / \ / \ / \
+        3  2 1  0 3  2 6  4
+       /\/\ /\/\ /\/\ /\/\
+       N N N N N N N N N N N N N N N N
+    */
 
     Node *t2 = new_node(new_node(new_node(leaf(3), 5, leaf(2)), 10, new_node(leaf(1), 4, leaf(8))), 20, new_node(new_node(leaf(3), 5, leaf(2)), 8, new_node(leaf(6), 6, leaf(4))));
     test_equal_b(is_monotone_tree(t2), false);
+    /*
+                20
+              /    \
+            10      8
+           /  \    / \
+          5    4  5   6
+         / \  / \ / \ / \
+        3  2 1  8 3  2 6  4
+       /\/\ /\/\ /\/\ /\/\
+       N N N N N N N N N N N N N N N N
+    */
 
     Node *t3 = new_node(new_node(new_node(leaf(3), 5, leaf(2)), 10, new_node(leaf(1), 4, leaf(0))), 20, new_node(new_node(leaf(3), 5, leaf(2)), 6, new_node(leaf(6), 7, leaf(4))));
     test_equal_b(is_monotone_tree(t3), false);
+    /*
+                20
+              /    \
+            10      6
+           /  \    / \
+          5    4  5   7
+         / \  / \ / \ / \
+        3  2 1  0 3  2 6  4
+       /\/\ /\/\ /\/\ /\/\
+       N N N N N N N N N N N N N N N N
+    */
 }
